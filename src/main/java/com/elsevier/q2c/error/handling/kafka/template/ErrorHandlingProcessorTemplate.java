60d729fed3be66841267ddd40f474664b73084cd
VassilisSpiliopoulos@Elsevier <38494470+vspiliopoulos@users.noreply.github.com>
2019-02-26 16:45:04 +0000

    non backwards compatible change to allow multiple streams to consume (#5)
    from from the same topic

--- src/main/java/com/elsevier/q2c/error/handling/kafka/template/ErrorHandlingProcessorTemplate.java
+++ src/main/java/com/elsevier/q2c/error/handling/kafka/template/ErrorHandlingProcessorTemplate.java
@@ -5,7 +5,6 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.KStream;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import com.elsevier.q2c.error.handling.kafka.compiler.ErrorHandlingStreamCompiler;
 import com.elsevier.q2c.error.handling.kafka.processor.ErrorHandlingProcessor;
@@ -14,10 +13,13 @@ public abstract class ErrorHandlingProcessorTemplate<FromValueType extends Speci
 				extends ErrorHandlingProcessor<FromValueType, SuccessValueType> 
 				implements ErrorHandlingProcessorTemplateInterface<FromValueType> {
 
-	@Autowired
-	ErrorHandlingStreamCompiler<FromValueType> streamCompiler;
+  ErrorHandlingStreamCompiler<FromValueType> streamCompiler;
 	
-	public KStream<String, ? extends SpecificRecord> createStreamForInitialTry(StreamsBuilder builderInitialTrySteamBuilder) {
+  public ErrorHandlingProcessorTemplate(ErrorHandlingStreamCompiler<FromValueType> streamCompiler) {
+    this.streamCompiler = streamCompiler;
+  }
+  
+  public KStream<String, ? extends SpecificRecord> createStreamForInitialTry(StreamsBuilder builderInitialTrySteamBuilder) {
 		KStream<String, FromValueType> kStream = builderInitialTrySteamBuilder.stream(getConsumeFrom(), 
 											Consumed.with(Serdes.String(), eventSerdeFrom));
 		// set the sourceTopic and serviceName on the compiler, so they may be used by the wraptoDlqEvent()
